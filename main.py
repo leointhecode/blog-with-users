cfb8cd04df29e2eec5d237b215d52b0f9640d604
lenny <lenny-IdeaPad.local>
2023-01-05 21:49:59 -0600

    2ND COMMIT

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask, render_template, redirect, url_for, flash, abort
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
@@ -11,8 +13,10 @@ from flask_gravatar import Gravatar
 from functools import wraps
 import smtpd
 
+load_dotenv()
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv("FLASK_SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
